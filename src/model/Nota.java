d7e6c07d757f75c4da8f3a417d5a9641aa07bbf4
Jeremi González Perera <gonzalezjeremi532@gmail.com>
2026-03-06 02:45:39 +0000

    Subida de codigo Nota.java y Usuario.java

--- src/model/Nota.java
+++ src/model/Nota.java
@@ -1,1 +1,24 @@
 package model;
+
+public class Nota {
+    private String titulo;
+    private String contenido;
+
+    public Nota(String titulo, String contenido) {
+        this.titulo = titulo;
+        this.contenido = contenido;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public String getContenido() {
+        return contenido;
+    }
+
+    // Método útil para guardar en el fichero
+    public String toFicheroString() {
+        return titulo + ";" + contenido;
+    }
+}
--- src/model/Usuario.java
+++ src/model/Usuario.java
@@ -1,5 +1,19 @@
 package model;
 
 public class Usuario {
-    
+    private String email;
+    private String password;
+
+    public Usuario(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
 }
